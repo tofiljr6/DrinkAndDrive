ee838759cc2ee8ca8e753744dbba74036c51b7e2
Cheta2000 <mati28500@gmail.com>
2021-06-01 12:54:26 +0200

    Diff user parameters, finished for diff users

---
+++ app/src/main/java/com/example/drinkdrive/database/Parameters.kt
@@ -0,0 +1,15 @@
+package com.example.drinkdrive.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName="parameters")
+data class Parameters(
+    @ColumnInfo(name="id") @PrimaryKey(autoGenerate = true) var id:Int,
+    @ColumnInfo(name="gender") var gender:String,
+    @ColumnInfo(name="weight") var weight:Float,
+    @ColumnInfo(name="height")var height:Float,
+    @ColumnInfo(name="userId") var userId:String
+)
