86d792bf71a953cd90f38321ae3719b71cd8eed4
Cheta2000 <mati28500@gmail.com>
2021-06-06 23:42:31 +0200

    Lay

--- app/src/main/java/com/example/drinkdrive/activities/CoctailActivity.kt
+++ app/src/main/java/com/example/drinkdrive/activities/CoctailActivity.kt
@@ -2,10 +2,14 @@ package com.example.drinkdrive.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.drinkdrive.R
+import com.firebase.ui.auth.ui.ProgressView
 import com.koushikdutta.ion.ImageViewBitmapInfo
 import com.koushikdutta.ion.Ion
 import org.json.JSONArray
@@ -19,12 +23,12 @@ class CoctailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coctail)
         imageView=findViewById(R.id.coctailImg)
-        Ion.with(this)
-            .load("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-            .asString()
-            .setCallback{e,result->
-                processData(result)
-            }
+        findViewById<Button>(R.id.button12).setOnClickListener {
+            generate()
+            true
+        }
+        generate()
+
     }
 
     fun processData(data:String){
@@ -34,11 +38,15 @@ class CoctailActivity : AppCompatActivity() {
         val ingredients= arrayListOf<String>()
         val measures= arrayListOf<String>()
         val ingr=findViewById<TextView>(R.id.ingredients)
+        ingr.text=""
         findViewById<TextView>(R.id.name).text=json.getString("strDrink")
         findViewById<TextView>(R.id.type).text=json.getString("strAlcoholic")
         findViewById<TextView>(R.id.glass).text=json.getString("strGlass")
         findViewById<TextView>(R.id.instructions).text=json.getString("strInstructions")
         val url=json.getString("strDrinkThumb")
+        //findViewById<>(R.id.progressView).setProgress(1)
+
+
         for(i in 1 until 15){
             val info=json.getString("strIngredient$i")
             val amount=json.getString("strMeasure$i")
@@ -55,5 +63,14 @@ class CoctailActivity : AppCompatActivity() {
             .load(url)
             .into(imageView)
 
+    }
+
+    fun generate() {
+        Ion.with(this)
+            .load("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+            .asString()
+            .setCallback{e,result->
+                processData(result)
+            }
     }
 }
